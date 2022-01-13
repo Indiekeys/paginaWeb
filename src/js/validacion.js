b36bca51f63d96a7487fbed4155c4ede94c20cef
Christian <christianfernandez.alu@iespacomolla.es>
2022-01-13 17:19:19 +0100

    Ahora a empezar la parte funcional de los productos y sus diseños.

--- src/js/validacion.js
+++ src/js/validacion.js
@@ -3,3 +3,17 @@
 export function validacionPass(pass,pass2){
    return pass === pass2;
 }
+
+export function printLogIn(){
+
+      document.getElementById("log_out").classList.add("d-none");
+      document.getElementById("login").classList.remove("d-none");
+      document.getElementById("register").classList.remove("d-none");
+}
+
+export function printLogOut(){
+
+      document.getElementById("log_out").classList.remove("d-none");
+      document.getElementById("login").classList.add("d-none");
+      document.getElementById("register").classList.add("d-none");
+}
