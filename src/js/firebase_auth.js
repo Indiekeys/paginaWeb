8fd1d18701646652d4cb216f7313645e3ee23e04
AitorMT <aitormunoz.alu@iespacomolla.es>
2022-01-17 16:55:14 +0100

    Revisión Codigo, se ha removido ciertas lineas sin uso y se han añadido nuevos code errors.

--- src/js/firebase_auth.js
+++ src/js/firebase_auth.js
@@ -1,6 +1,17 @@
 "use strict";
 import { app } from "./firebase.js";
-import {getAuth,createUserWithEmailAndPassword,onAuthStateChanged,AuthErrorCodes,GoogleAuthProvider,signInWithPopup,FacebookAuthProvider,setPersistence,signOut,browserLocalPersistence,signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  GoogleAuthProvider,
+  signInWithPopup,
+  FacebookAuthProvider,
+  setPersistence,
+  signOut,
+  browserLocalPersistence,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/9.6.2/firebase-auth.js";
 import * as validar from "./validacion.js";
 
 const auth = getAuth(app);
@@ -11,132 +22,127 @@ const divError = document.getElementById("divError");
 const messageError = document.getElementById("messageError");
 
 export const persistAccount = () => {
+  setPersistence(auth, browserLocalPersistence);
+};
 
-    const persistCredential = setPersistence(auth,browserLocalPersistence);
-}
-
-export const createAccount = async (email,pass) => {
-    try {
-        const createCredential = await createUserWithEmailAndPassword(auth,email,pass);
-        hideLoginError();
-        persistAccount();
-        correctAuth();
-
-    }catch (error){
-        showLoginError(error);
-    }
-}
+export const createAccount = async (email, pass) => {
+  try {
+    await createUserWithEmailAndPassword(auth, email, pass);
+    hideLoginError();
+    persistAccount();
+    correctAuth();
+  } catch (error) {
+    showLoginError(error);
+  }
+};
 
 export const hideLoginError = () => {
-    divError.style.display = "none";
-    messageError.innerHTML = "";
-
-}
+  divError.style.display = "none";
+  messageError.innerHTML = "";
+};
 
 export const showLoginError = (error) => {
-    divError.style.display = "block";
-    console.log(error.code);
-    //Optimizar
-    switch (error.code) {
-        case AuthErrorCodes.INVALID_EMAIL:
-            messageError.innerHTML = "Por favor introduzca un correo valido";
-            break;
-        case AuthErrorCodes.INVALID_PASSWORD:
-            messageError.innerHTML = "Error al introducir la contraseña";
-            break;
-        case AuthErrorCodes.USER_NOT_FOUND:
-            messageError.innerHTML = "El usuario no existe";
-            break;
-        case AuthErrorCodes.USER_DISABLED:
-            messageError.innerHTML = "El usuario está deshabilitado";
-            break;
-        case AuthErrorCodes.EMAIL_ALREADY_IN_USE:
-            messageError.innerHTML = "El correo ya está en uso";
-            break;
-        case AuthErrorCodes.WEAK_PASSWORD:
-            messageError.innerHTML = "La contraseña debe tener como mínimo 6 caracteres";
-            break;
-        default:
-            messageError.innerHTML = "Error desconocido";
-            break;
-    }
-
-
-}
+  divError.style.display = "block";
+  console.log(error.code);
+  //Optimizar
+  switch (error.code) {
+    case "auth/invalid-email":
+      messageError.innerHTML = "Por favor introduzca un correo valido";
+      break;
+    case "auth/wrong-password":
+      messageError.innerHTML = "La contraseña es incorrecta";
+      break;
+    case "auth/invalid-password":
+      messageError.innerHTML = "Contraseña incorrecta";
+      break;
+    case "auth/user-not-found":
+      messageError.innerHTML = "El usuario no existe";
+      break;
+    case "auth/user-disabled":
+      messageError.innerHTML = "El usuario está deshabilitado";
+      break;
+    case "auth/email-already-in-use":
+      messageError.innerHTML = "El correo ya esta en uso";
+      break;
+    case "auth/weak-password":
+      messageError.innerHTML =
+        "La contraseña debe tener como mínimo 6 caracteres";
+      break;
+    case "auth/popup-blocked":
+      messageError.innerHTML = "";
+      break;
+    case "auth/popup-closed-by-user":
+      messageError.innerHTML = "";
+      break;
+    case "auth/cancelled-popup-request":
+      messageError.innerHTML = "";
+      break;
+    case "auth/too-many-requests":
+      messageError.innerHTML =
+        "Hemos bloqueado todas las solicitudes de este dispositivo debido a una actividad inusual. Vuelve a intentarlo más tarde.";
+      break;
+    default:
+      messageError.innerHTML =
+        "Ha surgido un error inesperado, inténtelo de nuevo";
+      break;
+  }
+};
 
 export const authGoogle = async () => {
-
-    try {
-        const credentialGoogle = await signInWithPopup(auth, providerG);
-        hideLoginError();
-        persistAccount();
-        correctAuth();
-
-    }catch (error){
-        showLoginError(error);
-    }
-}
+  try {
+    await signInWithPopup(auth, providerG);
+    hideLoginError();
+    persistAccount();
+    correctAuth();
+  } catch (error) {
+    showLoginError(error);
+  }
+};
 
 export const authFacebook = async () => {
+  try {
+    await signInWithPopup(auth, providerF);
+    hideLoginError();
+    persistAccount();
+    correctAuth();
+  } catch (error) {
+    showLoginError(error);
+  }
+};
 
-    try {
-        const credentialFacebook = await signInWithPopup(auth, providerF);
-        hideLoginError();
-        persistAccount();
-        correctAuth();
-
-    }catch (error){
-        showLoginError(error);
-    }
-}
-
-export const signAccount = async (email,pass) => {
-
-    try {
-        const signCredential = await signInWithEmailAndPassword(auth, email, pass);
-        hideLoginError();
-        persistAccount();
-        correctAuth();
-
-    }catch (error){
-        showLoginError(error);
-    }
-}
+export const signAccount = async (email, pass) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, pass);
+    hideLoginError();
+    persistAccount();
+    correctAuth();
+  } catch (error) {
+    showLoginError(error);
+  }
+};
 
 export const correctAuth = () => {
-
-    onAuthStateChanged(auth, (user) => {
-        if (user != null) {
-            window.location.href = "/";
-        }
-
-    });
-
+  onAuthStateChanged(auth, (user) => {
+    if (user != null) {
+      window.location.href = "/";
+    }
+  });
 };
 
 export const comprobarAuth = () => {
-
-    onAuthStateChanged(auth, (user) => {
-        if (user != null) {
-            validar.printLogOut();
-        }else{
-            validar.printLogIn();
-        }
-
-    });
-
+  onAuthStateChanged(auth, (user) => {
+    if (user != null) {
+      validar.printLogOut();
+    } else {
+      validar.printLogIn();
+    }
+  });
 };
 
 export const log_out = async () => {
-
-    try {
-        const logOutAuth = await signOut(auth);
-    }catch (error){
-        console.log(error);
-    }
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.log(error);
+  }
 };
-
-
-
-
-
