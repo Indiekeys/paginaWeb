a4f753bf496d541cf0a4dba2a46690de6e219142
AitorMT <aitormunoz.alu@iespacomolla.es>
2022-02-02 17:43:47 +0100

    Inserción de imagen default y almacenamiento del nombre y apellido desde el auth.

--- src/js/index_register.js
+++ src/js/index_register.js
@@ -1,6 +1,7 @@
 "use strict";
 import * as script from "./firebase_auth.js";
 import * as validar from "./validacion.js";
+import {setDisplayName} from "./firebase_auth.js";
 
 
 window.onload = ()=>{
@@ -11,14 +12,17 @@ script.correctAuth();
 
     document.getElementById("registrar").addEventListener(
         "click",
-        (e) => {
-
+        async (e) => {
+            let nombre = document.getElementById("nombre").value;
+            let apellidos = document.getElementById("apellidos").value;
             let correo = document.getElementById("correo").value;
             let pass = document.getElementById("pass").value;
             let pass2 = document.getElementById("repeat_pass").value;
 
             if(validar.validacionPass(pass,pass2)) {
-                script.createAccount(correo,pass);
+                await script.createAccount(correo, pass);
+                await script.setDisplayName(nombre, apellidos);
+                await script.setDefaultImageProfile();
             }else{
                 script.showLoginError("error");
             }
@@ -28,18 +32,18 @@ script.correctAuth();
 
     document.getElementById("google").addEventListener(
         "click",
-        (e) => {
-
-            script.authGoogle();
+        async (e) => {
+            await script.authGoogle();
+            await script.setDefaultImageProfile();
         },
         false
     )
 
     document.getElementById("facebook").addEventListener(
         "click",
-        (e) => {
-
-            script.authFacebook();
+        async (e) => {
+            await script.authFacebook();
+            await script.setDefaultImageProfile();
         },
         false
     )
